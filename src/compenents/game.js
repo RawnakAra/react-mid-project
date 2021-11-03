2a5c7c7324f9e355d67a7fc7281252ca6abe63b3
RawnakAra <rawnak96@gmail.com>
2021-11-04 00:24:35 +0200

    almost there

--- src/compenents/game.js
+++ src/compenents/game.js
@@ -1,52 +1,104 @@
 import axios from "axios";
 import React, { useEffect } from "react";
 import Border from "./border";
+import Timer from "./Timer/timer";
+
 
 
 const Game = () => {
+    const [data, setData] = React.useState(null)
     const [steps, setstps] = React.useState('')
     let [stepNumber, setStepNumber] = React.useState(0)
-    const [size ,setSize] = React.useState('')
-    const [squares, setSquares] = React.useState(Array(36).fill(''))
+    const [size, setSize] = React.useState(6)
+    const [squares, setSquares] = React.useState(["",3,4,5,4,3,5,"","","","","",5,"","","","","",5,"","","","","",3,"","","","","",1,"","","","",""])
     const [show, setShow] = React.useState(false)
     const [hearts, setHearts] = React.useState(['❤️', '❤️', '❤️'])
     const [showBtn, setShowBtn] = React.useState('');
-    const [puzzle, setPuzzle] = React.useState(Array(36).fill(''))
+    const [puzzle, setPuzzle] = React.useState(
+        ["",3,4,5,4,3,
+            5,1, 1, 1, 1, 1,
+            5,1, 1, 1, 1, 1,
+            5,1, 1, 1, 1, 1,
+            3,0, 1, 1, 1, 0,
+            1,0, 0, 1, 0, 0]
+    )
     let [id, setId] = React.useState(1)
-   
-    React.useEffect(() => {
-        getPuzzleFromApi()
-    }, [id])
+    const [select, setSelect] = React.useState('')
+    // const { minutes = 0, seconds = 0} = MinSecs
+    // const [[mins, secs], setTime] = React.useState([minutes, seconds]);
+    //const sizeArray = [6,9,11,16]
 
-    const getPuzzleFromApi = async () => {
-        await axios.get(`https://617c34b6d842cf001711c2b7.mockapi.io/puzzle/${id}`)
+    // React.useEffect(() => {
+    //     getPuzzleFromApi()
+    // }, [id])
+
+    // const getPuzzleFromApi = async () => {
+    //     await axios.get(`https://617c34b6d842cf001711c2b7.mockapi.io/puzzle/${id}`)
+    //         .then(res => {
+    //             setPuzzle(res.data.puzzle)
+    //             setSquares(res.data.squares)
+    //             setstps(res.data.stepNum)
+    //             setSize(res.data.size)
+    //             //setData(res.data)
+    //             //console.log(res.data.size)
+    //         })
+    // }
+
+    React.useEffect(() => {
+        getAllData()
+    }, [])
+
+    const getAllData = async () => {
+        await axios.get(`https://617c34b6d842cf001711c2b7.mockapi.io/puzzle`)
             .then(res => {
-                setPuzzle(res.data.puzzle)
-                setSquares(res.data.squares)
-                setstps(res.data.stepNum)
-                setSize(res.data.size)
-                //console.log(res.data.size)
+
+                setData(res.data)
+                //console.log(res.data)
             })
     }
 
+    const handleCange = (e) => {
+        console.log(Number(e.target.value) + 1)
+        let found = data.find(u => {
+            if (u.size == Number(e.target.value) + 1) {
+                return u
+            }
+        })
+        setPuzzle(found.puzzle)
+        setSelect(Number(e.target.value) + 1)
+        setSquares(found.squares)
+        setstps(found.stepNum)
+        setSize(found.size)
+        setId(found.id)
+    }
+
     const nextLevel = () => {
-        if (stepNumber === steps){
-            setId(id +1)
+        if (stepNumber === steps) {
+            setId(Number(id) + 1)
+            let found = data.find(u => {
+                if (u.id == Number(id) + 1) {
+                    return u
+                }
+            })
+            setPuzzle(found.puzzle)
             setStepNumber(0)
+            setSquares(found.squares)
+            setstps(found.stepNum)
+            setSize(found.size)
             setHearts(['❤️', '❤️', '❤️'])
         }
-        }
+    }
 
     const test = (index) => {
         //if (stepNumber !== steps){
-        if (index > size && index !== (size) && index !== (size*2 ) && index !== (size*3 ) && index !== (size*4 )) {
+        if (index > size && index !== (size) && index !== (size * 2) && index !== (size * 3) && index !== (size * 4)) {
             let squareCopy = [...squares]
             if (showBtn === '⬛') {
                 if (puzzle[index] === 1) {
                     squareCopy[index] = '⬛'
                     stepNumber = stepNumber + 1
                     setStepNumber(stepNumber)
-                    console.log(stepNumber)
+                    //console.log(stepNumber)
                 } else {
                     if (hearts.length > 1) {
                         let copyHearts = [...hearts]
@@ -75,11 +127,6 @@ const Game = () => {
             }
             setSquares(squareCopy)
         }
-    
-    // }else 
-    // if (stepNumber === steps) {
-    //     setId(id +1)
-    // }
     }
 
     const xButtonHandler = () => {
@@ -89,22 +136,32 @@ const Game = () => {
         setShowBtn('⬛')
     }
 
+    //    const tick =()=>{
+
+    //    } 
+
 
     return (
         <div>
             <h1>Nonogram</h1>
-            <div className='hearts' >
-                {
-                    hearts.map((h, index) => {
-                        return <span key={index}>{h}</span>
-                    })
-                }
-            </div >
+            <div className='header' style={{ display: 'flex', justifyContent: 'space-around' }}>
+                <div className='timer' style={{ marginLeft: '100ox' }}>
+                    {/* <Timer/> */}
+                    <span >00:00</span> 
+                </div>
+                <div className='hearts' >
+                    {
+                        hearts.map((h, index) => {
+                            return <span key={index}>{h}</span>
+                        })
+                    }
+                </div >
+            </div>
             {
-                show ? 'Game over' :
-                    <div>
+                show ?<h3 style={{fontSize:'36px'}}>Game over 🤯🤯</h3>  :
+                    <div style={{display:'flex' , justifyContent: 'space-around' }}>
                         <div  >
-                        <Border squares={squares} costomFun={test} show={show} size={size}/>
+                            <Border squares={squares} costomFun={test} show={show} size={size} />
                         </div>
                         <div className='inputs'>
                             <div className='btns'>
@@ -112,14 +169,25 @@ const Game = () => {
                                 <input type='button' value={'⬛'} className='btn' onClick={blackButtonHandler} />
                             </div>
                             <div className='nextlevel'>
-                                <input type='button' value='Next Level' className='save' onClick={()=> nextLevel()} />
+                                <input type='button' value='Next Level' className='save' onClick={() => nextLevel()} />
                             </div>
+                        
+                        <div >
+                            <select className="select" id="dropdown"  value={select} onChange={(e) => handleCange(e)}>
+                                <option value='5' >Board Size</option>
+                                <option value="5" >5X5</option>
+                                <option value="8" >8X8</option>
+                                <option value="10">10X10</option>
+                                <option value="15">15X15</option>
+                            </select>
+                        </div>
                         </div>
                     </div>
             }
+
         </div>
 
     )
 }
-// (stepNumber === steps) ? setId(id +1) : id
+
 export default Game
--- src/compenents/selectedbord.js
+++ src/compenents/selectedbord.js
@@ -1,17 +1,16 @@
 import React from "react";
 import Square from "./square";
 
-const Border = ({squares,costomFun,show,size}) => {
+const SelectBord = ({squares,costomFun,show,select}) => {
     return (
-        <div className='container' style={{display : "grid" , gridTemplate:`repeat(${size},1fr)/repeat(${size},1fr)`}}>
+        <div className='container' style={{display : "grid" , gridTemplate:`repeat(${select},1fr)/repeat(${select},1fr)`}}>
             {
-                squares.length > 0 ?
                 squares.map((value , index) => {
                     return <Square val={value} key={index} ss={index} show={show} costomFun={()=>costomFun(index)}/>
-                }):'Loding'
+                })
             }
 
         </div>
     )
 }
-export default Border
+export default SelectBord
